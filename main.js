9b91b80e46c0cfa34e3efd62f9ef961457506ee2
Prajcurry091307 <77421995+Prajcurry091307@users.noreply.github.com>
2021-11-15 16:36:42 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 img="";
-
+var status="";   
 function preload()
 {
     img = loadImage('dog_cat.jpg');
@@ -9,14 +9,36 @@ function setup()
 {
     canvas = createCanvas(640,420);
     canvas.center();
+    objectDetector=mlt.objectDetector('cocossd',modelLoaded); 
 }
-
+function modelLoaded()
+{
+    console.log("Model Loaded")
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
 function draw()
 {
     image(img,0,0,500,200);
     fill("#ff0000");
-    text("Dog",50,80);
+    text("Dog",45,75);
     nofill();
     stroke("#ff0000");
-    rect(40,60, 500, 750)
+    rect(30,60, 450, 350);
+
+    fill("#0000ff")
+    text("Cat", 275, 295);
+    nofill();
+    stroke("#0000ff")
+    rect(250,80, 300,330);
+
+
+}
+
+function gotResults(error, results)
+{
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
 }
